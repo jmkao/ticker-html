7093a549a3d8f26952aaa1a38115fdd39f36a2e9
James Kao <jmk17@cornell.edu>
2022-02-19 18:32:12 -0800

    Initial commit

---
+++ data.js
@@ -0,0 +1,10 @@
+data = [
+  [
+    "MostAwesomeDJ",
+    "<img width='20' src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/256px-Instagram_icon.png'> @instagram",
+    "12:00 EST / 15:00 PST"
+  ],
+  ["MostAugustDJ","@instagram","12:00 EST / 15:00 PST"],
+  ["YourAwesomeDJ","@instagram","12:00 EST / 15:00 PST"],
+  ["MostAppearDJ","@instagram","12:00 EST / 15:00 PST"]
+]
